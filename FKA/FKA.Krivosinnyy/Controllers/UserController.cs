3782aa68d6002071860c4ad98e25601da9f84f2a
Fedor <fedor_ka@mail.ru>
2024-12-12 19:39:01 +0300

    Login доделать Logout - OK Сделал Вьюху

--- FKA/FKA.Krivosinnyy/Controllers/UserController.cs
+++ FKA/FKA.Krivosinnyy/Controllers/UserController.cs
@@ -3,6 +3,8 @@ using FKA.Krivosinnyy.BLL.ViewModels.User;
 using FKA.Krivosinnyy.DAL.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace FKA.Krivosinnyy.Controllers
 {
@@ -71,16 +73,52 @@ namespace FKA.Krivosinnyy.Controllers
         {
             return View();
         }
-        //[Route("Login")]
-        //[HttpPost]
-        //public async Task<IActionResult> Login(LoginViewModel model)
-        //{
+        [Route("Login")]
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _mapper.Map<User>(model);
+                User signedUser = _userManager.Users.FirstOrDefault(u => u.Email == model.Email);
+                if (signedUser == null)
+                {
+                    ModelState.AddModelError("Login", "Неверный логин");
+                }
+                var role = _userManager.GetRolesAsync(user).Result.FirstOrDefault();
+                if (role is null)
+                {
+                    if (signedUser.UserName == "Admin")
+                    {
+                        await _userManager.AddToRoleAsync(signedUser, "Admin");
+                    }
+                    else
+                    {
+                        var defaultRole = _roleManager.Roles.Where(r => r.Name != "Admin").FirstOrDefault();
+                        await _userManager.AddToRoleAsync(signedUser, "Admin");
+                    }
+                }
+                if (signedUser != null)
+                {
+                    var claims = new List<Claim>()
+                    {
+                        new Claim(ClaimsIdentity.DefaultNameClaimType, user.Email),
+                        new Claim(ClaimsIdentity.DefaultRoleClaimType, _userManager.GetRolesAsync(signedUser).Result.FirstOrDefault())
+                    };
+                    await _signInManager.SignInWithClaimsAsync(signedUser, isPersistent: false, claims);
+                }
+                else
+                {
 
-        //}
+                }
+            }
+            return RedirectToAction("Index", "Home");
+        }
         [Route("Logout")]
         [HttpGet]
         public async Task<IActionResult> Logout()
         {
+            await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
         }
     }
