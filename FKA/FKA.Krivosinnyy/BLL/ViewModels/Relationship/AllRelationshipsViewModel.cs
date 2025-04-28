89f87d4543b15b9368dab201c92787d1f234d324
Fedor <fedor_ka@mail.ru>
2025-04-28 23:24:10 +0300

    +AllRelationshipsViewModel

---
+++ FKA/FKA.Krivosinnyy/BLL/ViewModels/Relationship/AllRelationshipsViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace FKA.Krivosinnyy.BLL.ViewModels.Relationship
+{
+    public class AllRelationshipsViewModel
+    {
+        public List<RelationshipViewModel> Relationships { get; set; }
+    }
+}
